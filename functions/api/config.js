ad5653b5d50cbb727288f63dfb019996143d1a86
pharaoh001 <nikita.efimov0355@gmail.com>
2026-05-24 22:18:25 +0300

    Delete functions/api/config.js

--- functions/api/config.js
+++
@@ -1,2 +0,0 @@
-// URL Google Apps Script (см. GOOGLE-FORM-SETUP.md)
-window.FORM_ENDPOINT = "https://script.google.com/macros/s/AKfycbxBqRF2UKLQlp7QsBbKhPFuJh9SO0fh-D-1s8EW80-ssGLoPBmxJ24x6BIXbTEB0VIFqw/exec";
