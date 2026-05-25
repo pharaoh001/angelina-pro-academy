5334d9b980384cfc9e29f10ef1295414b01d257d
pharaoh001 <nikita.efimov0355@gmail.com>
2026-05-25 11:57:13 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // URL Google Apps Script (см. GOOGLE-FORM-SETUP.md)
-window.FORM_ENDPOINT = "https://script.google.com/macros/s/НОВЫЙ_ID/exec";
+window.FORM_ENDPOINT = "https://script.google.com/macros/s/AKfycby1VrsJwl1duU0O5PiECj6msfZ3Hbgj52VQwckviq0Zok9XEerknZSyvj_GDTCXlYS9WA/exec";
